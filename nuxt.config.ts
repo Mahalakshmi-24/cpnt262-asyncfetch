88c7091004b165085e6bc2080f9fb56dc01db402
Mahalakshmi <mahait07@gmail.com>
2022-03-30 18:45:04 -0600

    storyblok api connencted

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+import { defineNuxtConfig } from "nuxt3";
+ 
+export default defineNuxtConfig({
+
+  
+  
+  buildModules: [
+    ["@storyblok/nuxt", { accessToken: "3lMhuza7E6s5kpnWAX3NzAtt" }],
+  ]
+});
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Index page in Page directory</h1>
+
+    <pre> {{pageContent}}</pre>
+     </div>
+</template>
+
+<script setup>
+const storyapi = useStoryApi();
+const { data : home } = await storyapi.get("cdn/stories/home",
+ { version: "draft" });
+
+const state = reactive({ story: home.story });
+const pageContent =state.story;
+
+</script>
